1b8a94be368d117c8754bfae97797075efef2cc4
Jong Lee <jonglee1@illinois.edu>
2021-07-15 09:08:29 -0500

    make two test methods

--- test/test_samples.py
+++ test/test_samples.py
@@ -3,11 +3,7 @@ import json
 
 import requests
 
-
-def test_samples():
-    old_frag_file = open('old-fragility-set.json')
-    old_frag = json.load(old_frag_file)
-
+def test_new_samples():
     new_frag_file = open('new-fragility-set.json')
     new_frag = json.load(new_frag_file)
 
@@ -16,11 +12,15 @@ def test_samples():
     response = requests.post('http://localhost:5000/api/samples', json=new_frag, params=params)
     print(response.content)
 
-    # print("=== old fragility ===")
-    # params = {'sample_size':2}
-    # response = requests.post('http://localhost:5000/api/samples', json=old_frag, params=params)
-    # print(response.content)
+def test_old_samples():
+    old_frag_file = open('old-fragility-set.json')
+    old_frag = json.load(old_frag_file)
+
+    print("=== old fragility ===")
+    params = {'sample_size':2}
+    response = requests.post('http://localhost:5000/api/samples', json=old_frag, params=params)
+    print(response.content)
 
 
 if __name__ == '__main__':
-    test_samples()
+    test_new_samples()
