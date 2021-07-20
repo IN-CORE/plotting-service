8405d42fafc3094d09d84d206adf074a1f25e3d0
Yong Wook Kim <ywkim@illinois.edu>
2021-07-20 15:21:21 -0500

    added docker scripts

---
+++ release.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Exit on error
+set -e
+
+# use DEBUG=echo ./release-all.sh to print all commands
+export DEBUG=${DEBUG:-""}
+
+$DEBUG docker login hub.ncsa.illinois.edu
+
+# Find out what branch we are on
+BRANCH=${BRANCH:-"$(git rev-parse --abbrev-ref HEAD)"}
+
+# Find out the version
+if [ "$BRANCH" = "master" ]; then
+    VERSION=""
+elif [ "${BRANCH}" = "develop" ]; then
+    VERSION="-dev"
+else
+    exit 0
+fi
+
+# Push plotting service image
+docker push hub.ncsa.illinois.edu/incore/plotting$VERSION:latest
