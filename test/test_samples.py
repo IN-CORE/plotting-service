3571f45a43184970a38c5d3ad5e7bf8431995ff2
Chen Wang <cwang138@illinois.edu>
2021-10-20 13:23:23 -0500

    remove unused code

--- test/test_samples.py
+++ test/test_samples.py
@@ -1,8 +1,8 @@
-import pytest
 import json
 
 
 import requests
+
 
 def test_new_samples():
     new_frag_file = open('new-fragility-set.json')
@@ -11,15 +11,6 @@ def test_new_samples():
     print("=== new fragility ===")
     params = {'sample_size':2}
     response = requests.post('http://localhost:5000/api/samples', json=new_frag, params=params)
-    print(response.content)
-
-def test_old_samples():
-    old_frag_file = open('old-fragility-set.json')
-    old_frag = json.load(old_frag_file)
-
-    print("=== old fragility ===")
-    params = {'sample_size':2}
-    response = requests.post('http://localhost:5000/plotting/plotting/api/samples', json=old_frag, params=params)
     print(response.content)
 
 
