8b3d2f2d6c69a38175130dc62ea42a0e03025ab2
Chen Wang <cwang138@illinois.edu>
2021-10-27 13:40:12 -0500

    rename release to 1.6.0

--- plotting-service/config.py
+++ plotting-service/config.py
@@ -4,9 +4,9 @@ project = 'plotting-service'
 author = 'Chen Wang'
 
 # The short X.Y version
-version = '0.2.0'
+version = '1.6.0'
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '1.6.0'
 
 DATABASE = 'data/cache.sqlite3'
 RANGE = {
